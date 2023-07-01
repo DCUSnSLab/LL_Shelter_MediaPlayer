76f1c3ebf5f9bf3bfc5c036adae1793cf64ae679
soobin Jeon <>
2023-07-02 01:27:55 +0900

    test changed

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-import pygetwindow as gw
-
-print(gw.getWindowsWithTitle("Screen Title")[0].hwnd)
+a = "0"
+chek = int(a[0])
+try:
+    data = int(a[1:])
+except:
+    data = chek
+print(chek, data)
